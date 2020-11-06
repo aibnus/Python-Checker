a81664525a607d8fa945923a750573ae5c88138e
aibnus <ahmad.adi02@gmail.com>
2020-11-06 14:59:53 +0700

    first commit

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+import pygame
+
+#APPEARANCE
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+#COLORS (R,G,B)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
+
+#ASSETS
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (45, 25))
